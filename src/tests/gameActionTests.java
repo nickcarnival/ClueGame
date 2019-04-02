6a00f06b9c7269a4867f3dd04e03ea3f30aaa688
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-02 11:37:08 -0600

    added testSelectTargets()

--- src/tests/gameActionTests.java
+++ src/tests/gameActionTests.java
@@ -9,6 +9,38 @@ package tests;
  */
 import static org.junit.Assert.*;
 
-public class gameActionTests {
+import org.junit.Before;
+import org.junit.BeforeClass;
 
+import clueGame.Board;
+import clueGame.ComputerPlayer;
+import clueGame.Player;
+
+public class gameActionTests {
+	
+	private static Board board;
+	
+	@Before
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("data/map.csv", "data/rooms.txt");		
+		board.initialize();
+	}
+	public void testSelectTarget() {
+		ComputerPlayer NPC = new ComputerPlayer("red", "Jimothy Jenkins");
+
+		
+	}
+	/*
+		if no rooms in list, select randomly
+		if room in list that was not just visited, must select it
+		if room just visited is in list, each target (including room) selected randomly
+	 */
 }
+
+
+
+
+
