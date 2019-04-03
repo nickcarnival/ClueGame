5f53069f5f14e3c9945ff7923051ac59df7d9a60
Jordan Newport <jordannewport@mymail.mines.edu>
2019-04-03 15:04:36 -0600

    test for multiple unseen cards passes

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -49,23 +49,28 @@ public class ComputerPlayer extends Player {
 		return randomCell;
 		
 	}
-	//a suggestion is ...
+	
 	public Solution createSuggestion() {
 		Solution suggestion = new Solution();
+		Random random = new Random();
 		// set person to one we haven't seen
+		ArrayList<Card> people = new ArrayList<Card>();
 		for(Card c : board.getPeopleCards()) {
 			if(!seenCards.contains(c)) {
-				suggestion.setPerson(c);
-				break;
+				people.add(c);
 			}
 		}
+		suggestion.setPerson(people.get(random.nextInt(people.size())));
+
 		// set weapon to one we haven't seen
+		ArrayList<Card> weapons = new ArrayList<Card>();
 		for(Card c : board.getWeaponCards()) {
 			if(!seenCards.contains(c)) {
-				suggestion.setWeapon(c);
-				break;
+				weapons.add(c);
 			}
 		}
+		suggestion.setWeapon(weapons.get(random.nextInt(weapons.size())));
+
 		// set room to one we're in
 		char currentRoom = location.getInitial();
 		String room = board.getLegend().get(currentRoom);
