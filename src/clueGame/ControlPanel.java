f7726af441c1cc17f929c6b4e1fd5d2857447242
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-05 17:12:36 -0600

    added myCards to the ControlPanel gui

--- src/clueGame/ControlPanel.java
+++ src/clueGame/ControlPanel.java
@@ -43,32 +43,72 @@ public class ControlPanel extends JFrame{
 		
 	}
 	public void createLayout() {
+        JLabel board = new JLabel("board");
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
         JLabel label = new JLabel("File");
+
+        JPanel topPanel = new JPanel();
+        JPanel boardPanel = new JPanel();
+
+        JPanel cardPanel = new JPanel();
         JLabel whoseTurn = new JLabel("Whose turn? ");
         JLabel myCards = new JLabel("My Cards");
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        JPanel topPanel = new JPanel();
+        JPanel peopleCardPanel = new JPanel();
+        JLabel peopleCardLabel = new JLabel("People Cards");
+
+        JPanel weaponCardPanel = new JPanel();
+        JLabel weaponCardLabel = new JLabel("Weapon Cards");
+
+        JPanel roomCardPanel = new JPanel();
+        JLabel roomCardLabel = new JLabel("Room Cards");
+
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         JButton makeAccusationButton = new JButton("Make Accusation");
         JButton nextPlayerButton = new JButton("Next Player");
 
-        bottomPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        rightPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        topPanel.add(label);
+        
+        boardPanel.add(board);
+        
+        board.setPreferredSize(new Dimension(400, 400));
+        cardPanel.setPreferredSize(new Dimension(100, 680));
 
+        //setting border colors
+        board.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        cardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        weaponCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        peopleCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        roomCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        bottomPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+
+
+        //adding each type of card to the card panel
+        cardPanel.add(weaponCardPanel, BorderLayout.NORTH);
+        cardPanel.add(peopleCardPanel, BorderLayout.CENTER);
+        cardPanel.add(roomCardPanel, BorderLayout.SOUTH);
+
+        weaponCardPanel.add(weaponCardLabel);
+        peopleCardPanel.add(peopleCardLabel);
+        roomCardPanel.add(roomCardLabel);
+
+        cardPanel.add(whoseTurn);
+
+        //setting the button size
         makeAccusationButton.setPreferredSize(new Dimension(300, 50));
         nextPlayerButton.setPreferredSize(new Dimension(300, 50));
 
         bottomPanel.add(nextPlayerButton);
         bottomPanel.add(makeAccusationButton);
 
-
         mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(boardPanel, BorderLayout.WEST);
+        mainPanel.add(cardPanel, BorderLayout.EAST);
         mainPanel.add(bottomPanel, BorderLayout.SOUTH);
 
-        topPanel.add(label);
 
         add(mainPanel);
         setVisible(true);
