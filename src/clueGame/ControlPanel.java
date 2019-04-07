bb440e1476e268063a6b237f4758fcfa49c73090
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-06 20:48:23 -0600

    changed the bottom panel to a GridLayout

--- src/clueGame/ControlPanel.java
+++ src/clueGame/ControlPanel.java
@@ -8,6 +8,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 /*
  * This class controls all of the GUI
@@ -29,6 +31,7 @@ public class ControlPanel extends JFrame{
 	//these values go in the text fields
 	private String whoseTurnString = "Miss Scarlet";
 	private String diceValue = "4";
+	private String emptyString = "    ";
 
 	public ControlPanel() {
 
@@ -64,7 +67,6 @@ public class ControlPanel extends JFrame{
         JLabel board = new JLabel("board");
 
         JPanel mainPanel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("File");
 
         JPanel topPanel = new JPanel();
         
@@ -76,39 +78,45 @@ public class ControlPanel extends JFrame{
         JLabel myCards = new JLabel("My Cards");
 
         JPanel peopleCardPanel = new JPanel();
-        JLabel peopleCardLabel = new JLabel("People Cards");
         JPanel weaponCardPanel = new JPanel();
-        JLabel weaponCardLabel = new JLabel("Weapon Cards");
         JPanel roomCardPanel = new JPanel();
-        JLabel roomCardLabel = new JLabel("Room Cards");
 
         //whose turn
         JPanel whoseTurnPanel = new JPanel();
 
-        //this contains the actual text
+			//this contains the actual text
         JTextField whoseTurnField = new JTextField();
+        whoseTurnField.setEditable(false);
+        whoseTurnField.setText(whoseTurnString );
 
-        whoseTurnField.setText("Who's turn " + whoseTurnString );
-        JLabel whoseTurnLabel = new JLabel("Whose turn?");
 
         //buttons
         JButton nextPlayerButton = new JButton("Next Player");
         JButton makeAccusationButton = new JButton("Make Accusation");
 
         //dice
-        JPanel diePanel = new JPanel();
+        JPanel diePanel = new JPanel(new GridLayout(1,2));
+        JLabel diceLabel = new JLabel("Dice");
         JTextField diceRoll = new JTextField();
+        diceRoll.setEditable(false);
         diceRoll.setText(diceValue);
         
         //guess's
-        JPanel guessPanel = new JPanel();
+        JPanel guessPanel = new JPanel(new GridLayout(1,2));
 
-        JPanel guessResultPanel = new JPanel();
+        JPanel guessResultPanel = new JPanel(new GridLayout(1,2));
+        JTextField guessResult = new JTextField();
+        guessResult.setEditable(false);
 
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
+        //contains buttons and whoseturn
+        JPanel bottomPanel = new JPanel(new GridLayout(1, 3));
+        bottomPanel.add(diePanel);
+        bottomPanel.add(guessPanel);
+        bottomPanel.add(guessResultPanel);
 
-        topPanel.add(label);
+        //contains dice roll and guess
+        JPanel lowerBottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         
         boardPanel.add(board);
         
@@ -116,34 +124,40 @@ public class ControlPanel extends JFrame{
         cardPanel.setPreferredSize(new Dimension(150, 680));
 
         //setting border colors
-        board.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        cardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        weaponCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        peopleCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        roomCardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        diePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        guessPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        bottomPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        mainPanel.setBorder(new TitledBorder("File"));
+        cardPanel.setBorder(new TitledBorder("Card Panel"));
+        weaponCardPanel.setBorder(new TitledBorder("Weapons"));
+        peopleCardPanel.setBorder(new TitledBorder("People"));
+        roomCardPanel.setBorder(new TitledBorder("Rooms"));
+        diePanel.setBorder(new TitledBorder("Dice"));
+        guessPanel.setBorder(new TitledBorder("Guess"));
+        whoseTurnPanel.setBorder(new TitledBorder("Whose Turn"));
+        bottomPanel.setBorder(new TitledBorder("Bottom Panel"));
 
         //adding each type of card to the card panel
         cardPanel.add(weaponCardPanel, BorderLayout.NORTH);
         cardPanel.add(peopleCardPanel, BorderLayout.CENTER);
         cardPanel.add(roomCardPanel, BorderLayout.SOUTH);
 
-        weaponCardPanel.add(weaponCardLabel);
-        peopleCardPanel.add(peopleCardLabel);
-        roomCardPanel.add(roomCardLabel);
+        diePanel.add(diceLabel);
+        diePanel.add(diceRoll);
 
-        whoseTurnPanel.add(whoseTurnLabel);
-
-        bottomPanel.add(whoseTurnPanel);
+        whoseTurnPanel.add(whoseTurnField);
+        
+        guessPanel.add(guessResult);
 
         //setting the button size
         makeAccusationButton.setPreferredSize(new Dimension(300, 50));
         nextPlayerButton.setPreferredSize(new Dimension(300, 50));
 
+        bottomPanel.add(whoseTurnPanel, BorderLayout.SOUTH);
+        bottomPanel.add(guessPanel);
+        bottomPanel.add(guessResultPanel);
+
         bottomPanel.add(nextPlayerButton);
         bottomPanel.add(makeAccusationButton);
+
+        bottomPanel.add(lowerBottomPanel);
 
         mainPanel.add(topPanel, BorderLayout.NORTH);
         mainPanel.add(boardPanel, BorderLayout.WEST);
