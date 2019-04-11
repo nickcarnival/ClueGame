1c3bc46cad627d936ee343e750f4a1ac4897ef08
Jordan Newport <jordannewport@mymail.mines.edu>
2019-04-11 12:12:15 -0600

    Started work on detective notes dialog

---
+++ src/clueGame/DetectiveNotes.java
@@ -0,0 +1,29 @@
+package clueGame;
+
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.Window;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class DetectiveNotes extends JDialog {
+	private ArrayList<JCheckBox> playerCheckBoxes = new ArrayList<JCheckBox>();
+	private ArrayList<JCheckBox> roomCheckBoxes = new ArrayList<JCheckBox>();
+	private ArrayList<JCheckBox> weaponCheckBoxes = new ArrayList<JCheckBox>();
+	
+	private JMenu playerGuessMenu;
+
+	public DetectiveNotes() {
+		// TODO Auto-generated constructor stub
+	}
+}
